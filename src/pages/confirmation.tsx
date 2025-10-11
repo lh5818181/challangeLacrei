d14abc8c6a19d3c5512e285584e105e33b7c60d1
Luis <lh5818181@gmail.com>
2025-10-11 16:31:04 -0300

    codigo de exemplo para confirmation e diversity

---
+++ src/pages/confirmation.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Confirmation() {
+  return <div>Confirmação de cadastro</div>
+}
---
+++ src/pages/diversity.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Diversity() {
+  return <div>Formulário de diversidade</div>
+}
